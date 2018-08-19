c2d944c481797539555b3ef9ab60449be2bd107e
Travis CI User <travis@example.org>
2018-08-19 18:04:19 +0000

    auto generated javadoc

---
+++ RPGitems-reloaded/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"think.rpgitems"},{"l":"think.rpgitems.api"},{"l":"think.rpgitems.commands"},{"l":"think.rpgitems.data"},{"l":"think.rpgitems.item"},{"l":"think.rpgitems.power"},{"l":"think.rpgitems.power.impl"},{"l":"think.rpgitems.support"},{"l":"think.rpgitems.utils"},{"l":"think.rpgitems.utils.itemnbtapi"}]
