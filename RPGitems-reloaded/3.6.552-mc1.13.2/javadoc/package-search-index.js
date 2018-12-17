54752f7e267a00c4309a3331920ab4ff96cebe73
Librazy <im@librazy.org>
2018-12-17 13:48:15 +0800

    housekeeping

--- RPGitems-reloaded/3.6.552-mc1.13.2/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"think.rpgitems"},{"l":"think.rpgitems.api"},{"l":"think.rpgitems.data"},{"l":"think.rpgitems.item"},{"l":"think.rpgitems.power"},{"l":"think.rpgitems.power.impl"},{"l":"think.rpgitems.support"},{"l":"think.rpgitems.utils"}]
